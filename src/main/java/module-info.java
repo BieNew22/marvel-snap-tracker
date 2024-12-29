9d29243a08f69bde9a58edaf4db13acbf0f7bd6e
BieNew22 <newbie11004@gmail.com>
2024-12-29 14:50:49 +0900

    Feat: add jackson, dotenv

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,11 @@ module com.example.marvelsnaptracker {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires io.github.cdimascio.dotenv.java;
+    requires com.fasterxml.jackson.databind;
 
     opens com.example.marvelsnaptracker to javafx.fxml;
     exports com.example.marvelsnaptracker;
+    exports com.example.marvelsnaptracker.contoller;
+    opens com.example.marvelsnaptracker.contoller to javafx.fxml;
 }
