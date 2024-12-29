31590bbcdc25855ca36102535287bf6d53b47272
BieNew22 <newbie11004@gmail.com>
2024-12-29 19:46:25 +0900

    Docs: edit docstring

--- src/main/java/com/example/marvelsnaptracker/utils/EnvManager.java
+++ src/main/java/com/example/marvelsnaptracker/utils/EnvManager.java
@@ -3,7 +3,7 @@ package com.example.marvelsnaptracker.utils;
 import io.github.cdimascio.dotenv.Dotenv;
 
 /**
- * 환견 변수를 읽고 관리하는 클래스
+ * 환견 변수를 읽고 관리하는 클래스, Singleton
  */
 public class EnvManager {
     private static final EnvManager instance = new EnvManager();
@@ -11,6 +11,7 @@ public class EnvManager {
     public final String BASE_DIR;
 
     private EnvManager() {
+        // .env 로 부터 환견 변수를 읽어서 제공하는 역할.
         Dotenv dotenv = Dotenv.load();
 
         BASE_DIR = dotenv.get("BASE_DIR");
