b31729cec67877ddf18895702a2b02dd955c6fa2
BieNew22 <newbie11004@gmail.com>
2025-01-08 17:47:06 +0900

    Design: complete base design

--- src/main/java/com/example/marvelsnaptracker/MainApplication.java
+++ src/main/java/com/example/marvelsnaptracker/MainApplication.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.*;
 
@@ -13,6 +14,9 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 510, 700);
+
+        // TODO : 제목 표시줄 부분 나중에 HBOX를 추가하여 수정
+        // stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Marvel Snap");
         stage.setScene(scene);
         stage.setResizable(false);
