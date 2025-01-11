258afd007cc9852bdc86149aa1ea3d9f000ca282
BieNew22 <newbie11004@gmail.com>
2025-01-11 17:59:37 +0900

    !BREAKING CHANGE: change db schema, need to add card object

--- init.sql
+++ init.sql
@@ -15,15 +15,24 @@ CREATE TABLE IF NOT EXISTS deck (
     last_play TEXT
 );
 
+-- 카드 테이블 생성
+CREATE TABLE IF NOT EXISTS card {
+    name TEXT PRIMARY KEY,
+    cost INTEGER,
+    power INTEGER
+};
+
 -- 덱에 있는 카드 리스트 테이블 생성
-CREATE TABLE IF NOT EXISTS cards (
+CREATE TABLE IF NOT EXISTS deck_card (
     id INTEGER PRIMARY KEY,
     owner TEXT,
     name TEXT,
-    FOREIGN KEY (owner) REFERENCES deck(id)
+    FOREIGN KEY (owner) REFERENCES deck(id),
+    FOREIGN KEY (name) REFERENCES card(name)
 );
 
 INSERT INTO deck VALUES ('test', 'test-deck', 0.0, 0, '2025-01-10 10:41');
+INSERT INTO card VALUES ('Abomination', 9, 5);
 INSERT INTO cards(owner, name) VALUES ('test', 'Abomination');
 INSERT INTO cards(owner, name) VALUES ('test', 'Abomination');
 INSERT INTO cards(owner, name) VALUES ('test', 'Abomination');
