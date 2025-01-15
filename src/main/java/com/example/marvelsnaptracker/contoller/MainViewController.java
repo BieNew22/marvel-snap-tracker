2d0df5942d2d2c4afb16f9b3204f7f5b33d60d24
BieNew22 <newbie11004@gmail.com>
2025-01-15 13:50:26 +0900

    Feat: add close & minimizze button event

--- src/main/java/com/example/marvelsnaptracker/contoller/MainViewController.java
+++ src/main/java/com/example/marvelsnaptracker/contoller/MainViewController.java
@@ -3,8 +3,11 @@ package com.example.marvelsnaptracker.contoller;
 import com.example.marvelsnaptracker.MainApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import lombok.Setter;
 
 import java.io.IOException;
 
@@ -14,7 +17,30 @@ public class MainViewController {
     private VBox contentBox;
 
     @FXML
+    private Button minimizeButton;
+
+    @FXML
+    private Button closeButton;
+
+    @Setter
+    private Stage primaryStage;
+
+    @FXML
     public void initialize() {
+        // taskbar 버튼에 이벤트 추가
+        // 창 최소화 버튼 이벤트 추가
+        minimizeButton.setOnAction(e -> {
+            if (primaryStage != null)
+                primaryStage.setIconified(true);
+        });
+
+        // 창 닫기 버튼 이벤트 추가
+        closeButton.setOnAction(e -> {
+            if (primaryStage != null)
+                primaryStage.close();
+        });
+
+        // 시작 화면 로드 : 현재는 deck-list-view.fxml
         try {
             FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("deck-list-view.fxml"));
             ScrollPane screen = loader.load();
