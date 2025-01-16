83c4cc44424d85132d95acc86d6b118b3470b6b5
BieNew22 <newbie11004@gmail.com>
2025-01-16 17:44:58 +0900

    Feat: add css test data in init.sql

--- init.sql
+++ init.sql
@@ -32,8 +32,9 @@ CREATE TABLE IF NOT EXISTS deck_card (
     FOREIGN KEY (name) REFERENCES card(name) ON UPDATE CASCADE
 );
 
-INSERT INTO deck VALUES ('test1', 'TEST DECk', 0.0, 0, '2025-01-10 10:41');
-INSERT INTO deck VALUES ('test2', '한글 테스트', 0.0, 0, '2025-01-16 11:08');
+INSERT INTO deck VALUES ('test1', 'TEST DECk', 8.0, 1, '2025-01-10 10:41');
+INSERT INTO deck VALUES ('test2', '한글 테스트', 80.0, 10, '2025-01-16 11:08');
+INSERT INTO deck VALUES ('test3', 'TEST DECk', 100.0, 100, '2025-01-10 10:41');
 --INSERT INTO card VALUES ('Abomination', 5, 9);
 --INSERT INTO deck_card(owner, name) VALUES ('test', 'Abomination');
 --INSERT INTO deck_card(owner, name) VALUES ('test', 'Abomination');
